7565828a6c271a4b78723c05bdbd4c5c73adcb0c
jaeho-sim <jaeho.code@gmail.com>
2026-07-15 19:32:44 -0400

    Add unit test target

---
+++ MailApp/MailAppTests/MailAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  MailAppTests.swift
+//  MailAppTests
+//
+//  Created by Jaeho Sim on 2026-07-15.
+//
+
+import Testing
+
+struct MailAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
